99791e658d4edebf9a5304e651f60d3bd05d7c7e
rkhushi <khushi.cs.20@nitj.ac.in>
2022-06-08 21:36:56 +0530

    add files

---
+++ js/huffman/build_tree.js
@@ -0,0 +1,30 @@
+function build_tree(data){
+
+	var node_count = data.length;
+	
+	var queue = new PriorityQueue({ comparator: function(a, b) { return a.frequency - b.frequency; }});
+	
+	for(i = 0; i < node_count; i++){
+		queue.queue(data[i]);
+	}
+	
+	var id_counter = 1;
+	
+	for(i = 0; i < node_count-1; i++){
+		var new_node = {
+			"id": "0",
+			"name": "",
+			"children": [queue.dequeue(), queue.dequeue()],
+			"frequency": null,
+		};
+		
+		new_node.children[0].id = id_counter++;
+		new_node.children[1].id = id_counter++;
+		
+		new_node.frequency = new_node.children[0].frequency + new_node.children[1].frequency;
+		
+		queue.queue(new_node);
+	}
+	
+	return queue.dequeue();
+}
